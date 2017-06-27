9cac016577b4b8bc348a7460191cc1ff0f3a06e3
Gustavo Jose G. Villafuerte Martin <gjgvm1@hotmail.com>
2017-06-27 16:34:40 -0500

    Add files via upload

---
+++ picoyplaca/picoyplaca_predictor/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace picoyplaca_predictor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(323, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PICO Y PLACA - PREDICTOR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fecha :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Placa :";
+            // 
+            // label4
+            // 
+            this.label4.AutoEllipsis = true;
+            this.label4.BackColor = System.Drawing.Color.DimGray;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(0, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(422, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "INTRODUSCA FECHA Y PLACA";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 27);
+            this.button1.TabIndex = 4;
+            this.button1.Tag = "0";
+            this.button1.Text = "Consultar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 56);
+            this.textBox1.MaxLength = 10;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(120, 85);
+            this.textBox2.MaxLength = 10;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(123, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "dd/mm/aaaa";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(223, 56);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "dia de semana";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoEllipsis = true;
+            this.label7.BackColor = System.Drawing.Color.DimGray;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(0, 118);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(422, 18);
+            this.label7.TabIndex = 9;
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 158);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pico y Placa - Precictor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ picoyplaca/picoyplaca_predictor/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace picoyplaca_predictor
+{
+    public partial class Form1 : Form
+    {
+        private int[] array_predictor = { 5, 1, 1, 2, 2, 3, 3, 4, 4, 5 };
+        private string[] weekDays2 = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+        private int dayofweek = 0;
+        private int ndigit_placa = 0;
+        private int leng_placa = 0;
+        private string cdigit_placa = string.Empty;
+        private string string_date = DateTime.Now.ToShortDateString().Trim();
+        private DateTime date = DateTime.Now;
+        private bool autoriza_circulacion = false;
+        private int modo = 0;
+        private bool valid_placa = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = string_date;
+            textBox1.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (modo == 0)
+            {
+                if (string.IsNullOrEmpty(textBox1.Text.Trim()))
+                {
+                    MessageBox.Show("Fecha NO puede estar en blanco...", "Fecha", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    textBox1.Focus();
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(textBox2.Text.Trim()))
+                    {
+                        MessageBox.Show("Placa NO puede estar en blanco...", "Fecha", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        textBox2.Focus();
+                    }
+                    else
+                    {
+                        try
+                        {
+                            date = DateTime.Parse(textBox1.Text.Trim());
+                            dayofweek = (int)date.DayOfWeek;
+                            autoriza_circulacion = predictor(dayofweek, textBox2.Text.Trim());
+                            if (valid_placa)
+                            {
+                                modo = 1;
+                                label6.Text = weekDays2[dayofweek];
+                                label6.Visible = true;
+                                textBox1.Enabled = false;
+                                textBox2.Enabled = false;
+                                label7.Text = string.Format("Dia de parada : {0}", weekDays2[array_predictor[ndigit_placa]]);
+                                button1.Text = "Limpiar";
+                                if (autoriza_circulacion)
+                                {
+                                    label4.Text = "!! SI !! PUEDE CIRCULAR EN LA FECHA SELECCIONADA...";
+                                }
+                                else
+                                {
+                                    label4.Text = "!! NO !! PUEDE CIRCULAR EN LA FECHA SELECCIONADA...";
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Ultimo digito de la placa debe ser un valor numerico...", "Placa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                textBox2.Focus();
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("Fecha Invalida...", "Fecha", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            textBox1.Focus();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                modo = 0;
+                button1.Text = "Consultar";
+                valid_placa = true;
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                label7.Text = string.Empty;
+                label4.Text = "INTRODUSCA FECHA Y PLACA";
+                label6.Text = string.Empty;
+                label6.Visible = false;
+                textBox1.Text = string_date;
+                textBox2.Text = string.Empty;
+                textBox1.Focus();
+            }
+        }
+
+        public bool predictor(int ndayofweek,string splaca)
+        {
+            bool breturn = true;
+            leng_placa = splaca.Trim().Length;
+            cdigit_placa = splaca.Trim().Substring(leng_placa - 1, 1);
+            if (cdigit_placa == "0" | cdigit_placa == "1" | cdigit_placa == "2" | cdigit_placa == "3" | cdigit_placa == "4" | cdigit_placa == "5" | cdigit_placa == "6" | cdigit_placa == "7" | cdigit_placa == "8" | cdigit_placa == "9")
+            {
+                valid_placa = true;
+                ndigit_placa = int.Parse(cdigit_placa);
+                if (array_predictor[ndigit_placa].ToString() == ndayofweek.ToString())
+                {
+                    breturn = false;
+                }
+            }
+            else
+            {
+                valid_placa = false;
+            }
+            return breturn;
+        }
+    }
+}
